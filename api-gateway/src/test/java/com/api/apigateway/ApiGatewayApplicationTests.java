1850445aca449986efb740c1c1a8681061a0d114
adorabled4 <dhx2648466390@163.com>
2023-03-13 13:44:46 +0800

    初始化网关

---
+++ api-gateway/src/test/java/com/api/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
