c3525a6c298bb745bea1fdd4e51b6369581ad568
adorabled4 <dhx2648466390@163.com>
2023-04-12 13:08:10 +0800

    fix: 调整common模块通用类

--- api-common/src/main/java/com/dhx/apicommon/common/BaseResponse.java
+++ api-common/src/main/java/com/dhx/apicommon/common/BaseResponse.java
@@ -1,42 +1,61 @@
 package com.dhx.apicommon.common;
 
+import com.dhx.apicommon.common.exception.ErrorCode;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
-
-import java.io.Serializable;
 
 /**
- * @author dhx_
+ * @author adorabled4
  * @className BaseResponse
- * @date : 2023/03/13/ 12:28
+ * @date : 2023/04/10/ 14:21
  **/
 @Data
-public class BaseResponse<T> implements Serializable {
+public class BaseResponse<T> {
+
 
     private int code;
 
-    private T data;
+    private T data;//  controller 中的不同的方法返回值的类型不同
 
     private String message;
 
-    public BaseResponse(int code, T data, String message) {
+    private String description;
+
+
+    public BaseResponse(int code, T data, String message, String description) {
         this.code = code;
         this.data = data;
         this.message = message;
+        this.description = description;
     }
 
-
-    public BaseResponse(T data) {
-        this.code = HttpStatus.OK.value();
-        this.data = data;
-        this.message = "ok";
-    }
-
-    public BaseResponse(int code, T data) {
-        this(code, data, "");
+    public BaseResponse(int code, T data, String description) {
+        this(code, data, "", description);
     }
 
     public BaseResponse(ErrorCode errorCode) {
-        this(errorCode.getCode(), null, errorCode.getMessage());
+        this(errorCode.getCode(), null, errorCode.getMessage(), errorCode.getDescription());
+    }
+
+    public BaseResponse(ErrorCode errorCode, String description) {
+        this(errorCode.getCode(), null, errorCode.getMessage(), description);
+    }
+
+
+    public BaseResponse(ErrorCode errorCode, String message, String description) {
+        this(errorCode.getCode(), null, message, description);
+    }
+
+
+    /**
+     * 正常返回
+     *
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<T>(200, data, "ok", "");
     }
 }
+
+
