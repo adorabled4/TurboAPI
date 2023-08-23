ef8933e6d1e45bc4601627b7292cc53133376064
adorabled4 <dhx2648466390@163.com>
2023-08-23 21:35:49 +0800

    refactor: 更新服务地址相关配置以及Dockfile参数配置

--- api-core/src/main/java/com/dhx/apicore/config/RedissonConfig.java
+++ api-core/src/main/java/com/dhx/apicore/config/RedissonConfig.java
@@ -1,9 +1,9 @@
 package com.dhx.apicore.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.TransportMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,15 +14,26 @@ import org.springframework.context.annotation.Configuration;
  * @date : 2023/04/23/ 20:04
  **/
 @Configuration
+@Slf4j
 public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
 
     @Bean
     RedissonClient redissonClient(){
         Config config = new Config();
 //        config.setTransportMode(TransportMode.EPOLL); // 默认是NIO的方式
+        log.error(host+":"+port+password);
         config.useSingleServer()
                 //可以用"rediss://"来启用SSL连接，前缀必须是redis:// or rediss://
-                .setAddress("redis://8.130.79.173:6379").setPassword("r324fhh2n3084brj2w");
+                .setAddress("redis://"+ host+ ":"+port).setPassword(password);
         return Redisson.create(config);
     }
 }
