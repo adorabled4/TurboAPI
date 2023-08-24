8a4f6d27a61e758c07ab7408d23f606e39c888ce
adorabled4 <dhx2648466390@163.com>
2023-08-24 12:57:47 +0800

    feat(api-core): 新增logback配置以及SysLog注解

---
+++ api-core/src/main/java/com/dhx/apicore/common/annotation/SysLog.java
@@ -0,0 +1,15 @@
+package com.dhx.apicore.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SysLog {
+    /**
+     * 执行的内容
+     *
+     * @return
+     */
+    String value() default "";
+}
---
+++ api-core/src/main/java/com/dhx/apicore/util/ThrowUtil.java
@@ -0,0 +1,47 @@
+package com.dhx.apicore.util;
+
+
+import com.dhx.apicommon.common.exception.BusinessException;
+import com.dhx.apicommon.common.exception.ErrorCode;
+
+/**
+ * @author <a href="https://blog.dhx.icu/"> adorabled4 </a>
+ * @className ThrowUtil
+ * @date : 2023/07/05/ 15:08
+ **/
+public class ThrowUtil {
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param runtimeException
+     */
+    public static void throwIf(boolean condition, RuntimeException runtimeException) {
+        if (condition) {
+            throw runtimeException;
+        }
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode) {
+        throwIf(condition, new BusinessException(errorCode));
+    }
+
+    /**
+     * 条件成立则抛异常
+     *
+     * @param condition
+     * @param errorCode
+     * @param message
+     */
+    public static void throwIf(boolean condition, ErrorCode errorCode, String message) {
+        throwIf(condition, new BusinessException(errorCode, message));
+    }
+
+}
