09f9cd401e5a6ff08b35cb166ecf3b427645f7f9
adorabled4 <dhx2648466390@163.com>
2023-12-24 19:44:15 +0800

    feat(api-core): 升级SpringCloud版本,添加Nacos作为注册中心

--- api-core/src/main/java/com/dhx/apicore/ApiCoreApplication.java
+++ api-core/src/main/java/com/dhx/apicore/ApiCoreApplication.java
@@ -4,10 +4,14 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @MapperScan("com.dhx.apicore.mapper")
 @EnableDubbo
+@EnableDiscoveryClient
+@RefreshScope
 public class ApiCoreApplication {
 
     public static void main(String[] args) {
