3b7d39b7fe3a5a151bd8b3184f474ec911d7abe8
adorabled4 <dhx2648466390@163.com>
2023-12-27 15:04:17 +0800

    feat: 添加同步接口元信息接口实现,修改isAIGC属性

--- api-interface/src/main/java/com/dhx/apiinterface/ApiInterfaceApplication.java
+++ api-interface/src/main/java/com/dhx/apiinterface/ApiInterfaceApplication.java
@@ -1,5 +1,6 @@
 package com.dhx.apiinterface;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @MapperScan("com.dhx.apiinterface.mapper")
 @EnableDiscoveryClient
+@EnableDubbo
 public class ApiInterfaceApplication {
 
     public static void main(String[] args) {
