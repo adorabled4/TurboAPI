8db058bef1b3aaf4a9cd1f596726fdf135554d8b
adorabled4 <dhx2648466390@163.com>
2024-01-14 23:55:43 +0800

    refactor: 删除poet相关无用代码

--- api-interface/src/main/java/com/dhx/apiinterface/service/IPoetService.java
+++ api-interface/src/main/java/com/dhx/apiinterface/service/IPoetService.java
@@ -15,66 +15,8 @@ import java.util.List;
  * @date 2023-03-11
  */
 public interface IPoetService extends IService<Poet> {
-    /**
-     * 查询诗句
-     * 
-     * @param id 诗句主键
-     * @return 诗句
-     */
-    public Poet selectPoetById(Long id);
-
-    /**
-     * 查询诗句列表
-     * 
-     * @param poet 诗句
-     * @return 诗句集合
-     */
-    public List<Poet> selectPoetList(Poet poet);
-
-    /**
-     * 新增诗句
-     * 
-     * @param poet 诗句
-     * @return 结果
-     */
-    public int insertPoet(Poet poet);
-
-    /**
-     * 修改诗句
-     * 
-     * @param poet 诗句
-     * @return 结果
-     */
-    public int updatePoet(Poet poet);
-
-    /**
-     * 批量删除诗句
-     * 
-     * @param ids 需要删除的诗句主键集合
-     * @return 结果
-     */
-    public int deletePoetByIds(Long[] ids);
-
-    /**
-     * 删除诗句信息
-     * 
-     * @param id 诗句主键
-     * @return 结果
-     */
-    public int deletePoetById(Long id);
 
     PoetVO getPoetById(Long id);
 
     Long randomId();
-
-    BaseResponse<PoetVO> getPoetVO(long id);
-
-    /**
-     * 获取随机诗句
-     * @return
-     */
-    BaseResponse<PoetVO> getRandomPoetVo();
-
-
-    long getTotal();
 }
--- api-interface/src/main/java/com/dhx/apiinterface/service/impl/PoetServiceImpl.java
+++ api-interface/src/main/java/com/dhx/apiinterface/service/impl/PoetServiceImpl.java
@@ -36,12 +36,6 @@ public class PoetServiceImpl extends ServiceImpl<PoetMapper, Poet> implements IP
 
 
     @Override
-    public long getTotal() {
-        return poetMapper.getTotal();
-    }
-
-
-    @Override
     @Cacheable("randomPoet")
     public PoetVO getPoetById(Long id) {
         Poet poet = getById(id);
@@ -62,99 +56,5 @@ public class PoetServiceImpl extends ServiceImpl<PoetMapper, Poet> implements IP
         return id;
     }
 
-    @Override
-    public BaseResponse<PoetVO> getPoetVO(long id) {
-        String key = RedisConstant.RANDOM_POET_KEY + id;
-        String s = stringRedisTemplate.opsForValue().get(key);
-        if (StringUtils.isEmpty(s)) {
-            // 写入redis
-            Poet poet = poetMapper.selectPoetById(id);
-            while (poet == null) {
-                poet = poetMapper.selectPoetById(1L);
-            }
-            PoetVO poetVO = BeanUtil.copyProperties(poet, PoetVO.class);
-            s = JSONUtil.toJsonStr(poetVO);
-            stringRedisTemplate.opsForValue().set(key, s, RedisConstant.RANDOM_POET_TTL, TimeUnit.SECONDS);
-            return ResultUtil.success(poetVO);
-        } else {
-            PoetVO poetVO = JSONUtil.toBean(s, PoetVO.class);
-            return ResultUtil.success(poetVO);
-        }
-    }
 
-    @Override
-    public BaseResponse<PoetVO> getRandomPoetVo() {
-        long total = this.getTotal();
-        long id = (long) (Math.random() * total + 1);
-        while (id < 0 || id >= total) {
-            id = (long) (Math.random() * total + 1);
-        }
-        return this.getPoetVO(id);
-    }
-
-    /**
-     * 查询诗句
-     *
-     * @param id 诗句主键
-     * @return 诗句
-     */
-    @Override
-    public Poet selectPoetById(Long id) {
-        return poetMapper.selectPoetById(id);
-    }
-
-    /**
-     * 查询诗句列表
-     *
-     * @param poet 诗句
-     * @return 诗句
-     */
-    @Override
-    public List<Poet> selectPoetList(Poet poet) {
-        return poetMapper.selectPoetList(poet);
-    }
-
-    /**
-     * 新增诗句
-     *
-     * @param poet 诗句
-     * @return 结果
-     */
-    @Override
-    public int insertPoet(Poet poet) {
-        return poetMapper.insertPoet(poet);
-    }
-
-    /**
-     * 修改诗句
-     *
-     * @param poet 诗句
-     * @return 结果
-     */
-    @Override
-    public int updatePoet(Poet poet) {
-        return poetMapper.updatePoet(poet);
-    }
-
-    /**
-     * 批量删除诗句
-     *
-     * @param ids 需要删除的诗句主键
-     * @return 结果
-     */
-    @Override
-    public int deletePoetByIds(Long[] ids) {
-        return poetMapper.deletePoetByIds(ids);
-    }
-
-    /**
-     * 删除诗句信息
-     *
-     * @param id 诗句主键
-     * @return 结果
-     */
-    @Override
-    public int deletePoetById(Long id) {
-        return poetMapper.deletePoetById(id);
-    }
 }
